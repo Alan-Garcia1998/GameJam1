1b6a82b4c73daa38a5e95dcb413c20a0ddacee19
Alan Garcia <argar484@gmail.com>
2019-01-31 00:33:37 -0700

    build

---
+++ GameJam1/Assets/Scenes/Screipts/CameraControlls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlls : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    private float spin = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        // transform.Rotate(new Vector3(40, 0, 0));
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         if (Input.GetKey("e"))
+         {
+             transform.Rotate(new Vector3(0, 0 + spin, 0 ));
+
+         }
+         if (Input.GetKey("q"))
+         {
+             transform.Rotate(new Vector3(0, 0 - spin, 0));
+         }
+         */
+    }
+}
